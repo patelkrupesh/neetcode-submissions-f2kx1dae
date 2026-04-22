dc6661b61fdd6d43963b83e0d941ac5aac02454b
patelkrupesh <patelkrupesh108@gmail.com>
2026-04-22 14:30:11 +0100

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    private boolean findNumber(int[][] matrix, int target, int l, int r, int y) {
+        int mid = (l + r) / 2;
+        int midNum = matrix[mid/y][mid % y];
+        if(target == midNum) return true;
+        if(l >= r) return false;
+
+        if(target < midNum)
+            return findNumber(matrix, target, l, mid-1, y);
+        else
+            return findNumber(matrix, target, mid+1, r, y);
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int l = 0;
+        int x = matrix.length;
+        if(x == 0) return false;
+
+        int y = matrix[0].length;
+        int len = x*y;
+        return findNumber(matrix, target, l, len-1, y);
+    }
+}
