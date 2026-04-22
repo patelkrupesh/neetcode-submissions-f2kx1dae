7ea4fbb317cb5620f0c2a8dd85511e6485711d3b
patelkrupesh <patelkrupesh108@gmail.com>
2026-04-22 17:42:02 +0100

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    private boolean isValid(int[] piles, int hours, int bananasPerHour){
+        int counter = 0;
+        for(int n : piles){
+            counter += ( n + bananasPerHour - 1 ) / bananasPerHour;
+        }
+//        System.out.println("counter : " + counter + " , hours : " + hours);
+        return counter <= hours;
+    }
+
+    private int binarySearch(int[] piles, int h, int l, int r) {
+        int mid = (l + r) / 2;
+//        System.out.println("l : " + l + " r : " + r + " mid : "+ mid );
+        if(l>=r) return r;
+        if(isValid(piles, h, mid)){
+            return binarySearch(piles, h, l, mid);
+        }else{
+            return binarySearch(piles, h, mid+1, r);
+        }
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int finalRes = 0;
+        int n = piles.length;
+        if(n == 0 || h <= 0) return 0;
+        List<Integer> list = new ArrayList<Integer>(n);
+        for (int i : piles) {
+            list.add(i);
+        }
+        Collections.sort(list);
+        int max = list.get(list.size()-1);
+        int min = 1;
+        return binarySearch(piles, h, min, max);
+    }
+}
