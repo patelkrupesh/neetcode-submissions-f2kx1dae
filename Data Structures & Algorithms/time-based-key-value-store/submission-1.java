30dc2dc88b38562ccabcb30681f1323552e614a0
patelkrupesh <patelkrupesh108@gmail.com>
2026-05-30 10:27:50 +0100

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.java
@@ -0,0 +1,55 @@
+class TimeMap {
+    public class Node{
+        String value;
+        int timestamp;
+        public Node(String value, int timestamp){
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+        @Override 
+        public String toString(){
+            return "[ value : " + value + " , timestamp : "+ timestamp + "]";
+        }
+    }
+    Map <String, List<Node>> map;
+
+    public TimeMap() {
+        map = new HashMap();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!map.containsKey(key)){
+            map.put(key, new ArrayList<>());
+        }
+        int size = map.get(key).size();
+        if(size == 0){
+            map.get(key).add(new Node(value, timestamp));
+            return;
+        }
+        Node n = map.get(key).get(size - 1);
+        if(n.timestamp == timestamp){
+            n.value = value;
+            map.get(key).set(size - 1, new Node(value, timestamp));
+        }else if (n.timestamp < timestamp){
+            map.get(key).add(new Node(value, timestamp));
+        } else{
+            System.out.println("Invalid timestamp, it must be stictly in increasing order.");
+        }
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!map.containsKey(key)){
+            return "";
+        }
+        List<Node> list = map.get(key);
+        if(list == null || list.size() == 0 ){
+            return "";
+        }
+        for (int i = list.size()-1 ; i>= 0 ; i--){
+            if(timestamp >= list.get(i).timestamp){
+                return list.get(i).value;
+            }
+        }
+        return "";
+    }
+}
